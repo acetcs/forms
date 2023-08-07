83f1b892b66d89304e93305b12d4389ed2892816
saifrahmn <saifrahmn78@gmail.com>
2023-08-07 21:45:51 +0530

    connecting with databse

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-import { initializeApp } from "firebase/app";
+const form = document.getElementById('contactForm');
+const alert = document.querySelector(".alert");
 
-
-
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
-// Initialize Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBQyE5A32XOQBT_rf3Q2OOnjvChvP5H2jY",
     authDomain: "project-terminal-a4511.firebaseapp.com",
@@ -21,39 +10,35 @@ const firebaseConfig = {
     messagingSenderId: "121180104722",
     appId: "1:121180104722:web:1cb43dc4faf90dde77d0cb",
     measurementId: "G-YYFY6S288R"
-};
-const form = document.getElementById('contactForm');
-
+  };
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
  
   
-  const database = firebase.database();
+  const database = firebase.database()
 
-  const ref = database.ref("Users")
+  const ref = database.ref("messages")
 
 
 form.addEventListener("submit",(e)=>{
     
     e.preventDefault();
     
-    const name = document.getElementById('firstname').value;
+    const firstname = document.getElementById('firstname').value;
     const lastname = document.getElementById('lastname').value;
-    const phone = document.getElementById('phone').value;
     const department = document.getElementById('department').value;
+    const phone = document.getElementById('phone').value;
     const git = document.getElementById('git').value;
-
     
     ref.push({
-        name:name,
+        firstname:firstname,
         lastname:lastname,
-        phone:phone,
         department:department,
+        phone:phone,
         git:git
     })
 
-    
 
     form.reset()
 
