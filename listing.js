61b0f64cdc221800e6a6caabe97a04e52f51f68b
saifrahmn <saifrahmn78@gmail.com>
2023-08-08 17:55:12 +0530

    adding the list maintainer

---
+++ listing.js
@@ -0,0 +1,321 @@
+const jsonData = {
+    // ... (your JSON data here)
+    
+        "messages": {
+          "-NbFwMSZMPsCOyScXGj4": {
+            "department": "cse-4",
+            "firstname": "Saif",
+            "git": "saifrahmn",
+            "lastname": "Rahman",
+            "phone": "8600264697"
+          },
+          "-NbFwvrpkD24tYL5g1Xi": {
+            "department": "CSE & 3 Yr",
+            "firstname": "Gufran",
+            "git": "https://GitHub.com/gufransiddique",
+            "lastname": "Siddique",
+            "phone": "07758868898"
+          },
+          "-NbFx5d-jn7Y2xM4uZOa": {
+            "department": "CSE & 3 Yr",
+            "firstname": "Gufran",
+            "git": "https://GitHub.com/gufransiddique",
+            "lastname": "Siddique",
+            "phone": "07758868898"
+          },
+          "-NbFxKVWVzHW5P0EWAPT": {
+            "department": "Cse-3",
+            "firstname": "Sufiya",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbFxS1us1fY0iFPJ8ni": {
+            "department": "Cse-3",
+            "firstname": "Sufiya",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbFxZRIDb5gZY3Glmfp": {
+            "department": "CSE-3",
+            "firstname": "Janvi",
+            "git": "Zadejanvi",
+            "lastname": "Zade",
+            "phone": "7219272919"
+          },
+          "-NbFxZhmUviqZHJV624I": {
+            "department": "Cse-3",
+            "firstname": "Sufiya",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbFxgyYY52mDjnCSeqL": {
+            "department": "CSE-3",
+            "firstname": "Janvi",
+            "git": "Zadejanvi",
+            "lastname": "Zade",
+            "phone": "7219272919"
+          },
+          "-NbFxthAr6RMPvxt6uyi": {
+            "department": "abhayzingre@gmail.com",
+            "firstname": "Abhay",
+            "git": "Abhay2546",
+            "lastname": "Zingre",
+            "phone": "8263052220"
+          },
+          "-NbFxuDiSK_S8k8CYR5D": {
+            "department": "Cse 4",
+            "firstname": "Saniya",
+            "git": "Dekhlo tumlog kya h to",
+            "lastname": "Quazi",
+            "phone": "7249834796"
+          },
+          "-NbFxxCMZlLMIVVa9S4z": {
+            "department": "abhayzingre@gmail.com",
+            "firstname": "Abhay",
+            "git": "Abhay2546",
+            "lastname": "Zingre",
+            "phone": "8263052220"
+          },
+          "-NbFyNOVHyDqU1L8qcdv": {
+            "department": "cse-3",
+            "firstname": "Aaliya",
+            "git": "Aaliyaarshiya",
+            "lastname": "Arshiya",
+            "phone": "9322786598"
+          },
+          "-NbFyWrQo7yuuH-4M0ea": {
+            "department": "CSE-3",
+            "firstname": "Kaushik",
+            "git": "https://github.com/Kaushikganvir",
+            "lastname": "Ganvir",
+            "phone": "9637855576"
+          },
+          "-NbFytBPWAMPV6E7PA1u": {
+            "department": "Cse-4",
+            "firstname": "Sakshi",
+            "git": "Sakshi_patil",
+            "lastname": "Patil",
+            "phone": "7499272402"
+          },
+          "-NbFz9Yff9ysLZY0ozl7": {
+            "department": "Cse-4",
+            "firstname": "Aquib",
+            "git": "Https://GitHub.com/aquibdarain",
+            "lastname": "Darain",
+            "phone": "9370805845"
+          },
+          "-NbFzOjeo3pk5JQmHZ6G": {
+            "department": "CSE-5",
+            "firstname": "Noman",
+            "git": "abeerali7933",
+            "lastname": "Ali",
+            "phone": "9096406337"
+          },
+          "-NbFzSsBL1lbD2jtjmst": {
+            "department": "CSE-3",
+            "firstname": "Kaushik",
+            "git": "https://github.com/Kaushikganvir",
+            "lastname": "Ganvir",
+            "phone": "9637855576"
+          },
+          "-NbG-GJXlwIrECRFTOJw": {
+            "department": "CSE -3",
+            "firstname": "Anas ",
+            "git": "https://github.com/Anasalattari",
+            "lastname": "Khan",
+            "phone": "8329267693"
+          },
+          "-NbG-OIUSC9vEii-6HSq": {
+            "department": "CSE-4",
+            "firstname": "Qaem",
+            "git": "https://github.com/qaem101",
+            "lastname": "Raza",
+            "phone": "7821023611"
+          },
+          "-NbG-OZXKaFa8MaF9urI": {
+            "department": "CSE -4",
+            "firstname": "Anuj ",
+            "git": "Anujthakur6527",
+            "lastname": "Thakur ",
+            "phone": "6203818871"
+          },
+          "-NbG-mFtXHc3sVfSICXW": {
+            "department": "CSE -3",
+            "firstname": "Anas ",
+            "git": "https://github.com/Anasalattari",
+            "lastname": "Khan",
+            "phone": "8329267693"
+          },
+          "-NbG0ROVxzUwYhdgsrvO": {
+            "department": "Cse-3",
+            "firstname": "Sai Uday Kiran ",
+            "git": "Uday-Kiran03",
+            "lastname": "Adakki",
+            "phone": "7796804323"
+          },
+          "-NbG1Ei9_WXk7aPyVaxS": {
+            "department": "Cse-3",
+            "firstname": "Sufiya",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbG25u5ZJevggxTN3Yc": {
+            "department": "Cse-3",
+            "firstname": "Sufiya ",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbG4MBKGlC39V-wDwpu": {
+            "department": "CSE-3",
+            "firstname": "Mohammad Asif",
+            "git": "https://github.com/asif-khan-2k19",
+            "lastname": "Khan",
+            "phone": "8956742601"
+          },
+          "-NbG4Z0mMQ1x2Mie0lY2": {
+            "department": "CSE 3rd",
+            "firstname": "Aryan",
+            "git": "Aryan",
+            "lastname": "Bangde",
+            "phone": "9607854498"
+          },
+          "-NbG6JQq82KL_reLvF2Q": {
+            "department": "CSE",
+            "firstname": "Mafaz",
+            "git": "https://github.com/Mafazahmad",
+            "lastname": "Ahmad",
+            "phone": "9369751045"
+          },
+          "-NbG6UgNRxI9iGs_Kg15": {
+            "department": "CSE-3",
+            "firstname": "Mafaz ",
+            "git": "https://github.com/Mafazahmad",
+            "lastname": "Ahmad ",
+            "phone": "9369751045"
+          },
+          "-NbGAjGG_fzoSRhQloNC": {
+            "department": "Cse-3",
+            "firstname": "Samiksha",
+            "git": "https://github.com/Shahanesamiksha",
+            "lastname": "Shahane",
+            "phone": "9970112604"
+          },
+          "-NbGZIlZP-4T9lKUtDOB": {
+            "department": "CSE-3",
+            "firstname": "Insherah ",
+            "git": "I don't have id",
+            "lastname": "Ahewer ",
+            "phone": "7741974793"
+          },
+          "-NbGoeX24lAJwilvA9Va": {
+            "department": "Department: CSE, Year:3rd, semester:5th sem ",
+            "firstname": "Tausif ",
+            "git": "TausifAlii",
+            "lastname": "Ali",
+            "phone": "9175837710"
+          },
+          "-NbHvB5suJzmC3LhVBY-": {
+            "department": "Cse- 3",
+            "firstname": "Umme",
+            "git": "Fae1q",
+            "lastname": "Hamza",
+            "phone": "7972971714"
+          },
+          "-NbIAk_Rsa1E_a5HWnU_": {
+            "department": "Cse-3",
+            "firstname": "Sufiya",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbIAtSaLFd4K2mYrnIb": {
+            "department": "Cse-3",
+            "firstname": "Sufiya",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbIBStkd8aFJV2JXYjg": {
+            "department": "Cse-3",
+            "firstname": "Sufiya",
+            "git": "https://github.com/sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbIKQxnLV_fDmGZAbmX": {
+            "department": "CSE-B",
+            "firstname": "Mohammad Taukib ",
+            "git": "Nagsenvan Raza chauk Dr. Amnnedkar Marg S.Oplot no.787",
+            "lastname": "Ansari",
+            "phone": "9665453629"
+          },
+          "-NbIKWTVgoml_KDfHYAR": {
+            "department": "CSE-B",
+            "firstname": "Mohammad Taukib ",
+            "git": "Nagsenvan Raza chauk Dr. Amnnedkar Marg S.Oplot no.787",
+            "lastname": "Ansari",
+            "phone": "9665453629"
+          },
+          "-NbIcoOtUS5_J1MIgN_l": {
+            "department": "cse-3",
+            "firstname": "Sufiya",
+            "git": "sufiykhan",
+            "lastname": "Khan",
+            "phone": "8766455573"
+          },
+          "-NbIiBFXvwzSMDi_9c66": {
+            "department": "Cse-3",
+            "firstname": "Divyani",
+            "git": "divyaninikam16@gmail.com",
+            "lastname": "Nikam",
+            "phone": "7558481811"
+          },
+          "-NbIiIMTz-vTPEol8zou": {
+            "department": "Cse-3",
+            "firstname": "Divyani",
+            "git": "divyaninikam16@gmail.com",
+            "lastname": "Nikam",
+            "phone": "7558481811"
+          },
+          "-NbIiSPvFlRLByb_yNWj": {
+            "department": "CSE 3rd year ",
+            "firstname": "Akanksha",
+            "git": "Akanksha Jambhulkar ",
+            "lastname": "Jambhulkar",
+            "phone": "9209018406"
+          },
+          "-NbIz-x6RTJ6JTZ-0Ly4": {
+            "department": "Cse 3rd year",
+            "firstname": "Aryan",
+            "git": "aryan",
+            "lastname": "Rahulwar",
+            "phone": "9370193765"
+          }
+        }
+      };
+  
+  function isPhoneNumberUnique(phoneNumber, jsonData) {
+    for (const key in jsonData.messages) {
+      if (jsonData.messages.hasOwnProperty(key)) {
+        if (jsonData.messages[key].phone === phoneNumber) {
+          return false; // Phone number is not unique
+        }
+      }
+    }
+    return true; // Phone number is unique
+  }
+  
+  const phoneNumberToCheck = '9370805845'; // Replace with the phone number you want to check
+  
+  const isUnique = isPhoneNumberUnique(phoneNumberToCheck, jsonData);
+  
+  if (isUnique) {
+    console.log(`${phoneNumberToCheck} is a unique phone number.`);
+  } else {
+    console.log(`${phoneNumberToCheck} is not unique.`);
+  }
